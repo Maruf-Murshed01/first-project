4f820f1815798c1590834ad2c592d902c2b1c002
Maruf Murshed <marufmurshed9255@gmail.com>
2023-12-18 05:44:10 +0600

    create find delete done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
-});
+//application routes
+// /api/v1/students/create-student
+app.use('/api/v1/students', StudentRoutes);
+
+// app.get('/', (req: Request, res: Response) => {
+//   const a = 10;
+//   res.send(a);
+// });
 
 export default app;
